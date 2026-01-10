4facc892749251779d629a5f16e2ee0827b0de21
unknown <anshikamishra1906@gmail.com>
2026-01-10 14:46:49 +0530

    AreaOfaCircle

--- Basic-Programmes/AddTwoNumbers.java
+++ Basic-Programmes/AddTwoNumbers.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Sum{
+public class AddTwoNumbers{
 public static void main(String[] args){
 Scanner scanner = new Scanner(System.in);
 int x = scanner.nextInt();
