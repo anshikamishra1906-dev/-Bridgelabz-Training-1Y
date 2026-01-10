e907a3d9793b20aac64c8c0a7dd5e42fcc6b992d
unknown <anshikamishra1906@gmail.com>
2026-01-10 13:02:30 +0530

    Celsius to Fahrenheit Conversion

---
+++ Basic-Programmes/Celsius to Fahrenheit Conversion.java
@@ -0,0 +1,10 @@
+import java.until.Scanner;
+public class CelsiustoFahrenheitConversion{
+public static void main(String[] args){
+scanner scanner = new scanner(System.in);
+System.out.print("Enter temperature in Fahrenheit : ");
+float celcius =  scanner.nextFloat();
+float Conversion = (Celsius * 9/5) + 32;
+System.out.println("Temperature in Fahrenheit : "+ Fahrenheit);
+}
+}
