b6399d679bd17fb7999436fe0b7ce25302651306
unknown <anshikamishra1906@gmail.com>
2026-01-16 00:01:40 +0530

    Double_Operation

--- Basic-Programmes/Double_Operation.java
+++ Basic-Programmes/Double_Operation.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class DoubleOperation{
+public class Double_Operation{
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 	System.out.print("Enter a:");
