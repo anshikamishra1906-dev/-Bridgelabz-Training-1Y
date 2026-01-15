b47649cd5cdddd20f6449d7b02bcbdce2b7b61ab
unknown <anshikamishra1906@gmail.com>
2026-01-15 19:42:33 +0530

    SamAverageMarks

---
+++ Basic-Programmes/SamAverageMarks.java
@@ -0,0 +1,10 @@
+public class SamAverageMarks{
+    public static void main(String[] args){
+	int Maths_marks = 94;
+	int Physics_marks = 95;
+	int Chemistry_marks = 96;
+	float average_marks =(94+95+96)/ 3;
+	System.out.println("Sam’s average mark in PCM is:"+ average_marks);
+	}
+}
+	
