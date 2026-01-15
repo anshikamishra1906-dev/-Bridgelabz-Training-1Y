2e8694d4f9d472cb5cdba7f39bfcfad7f8401d70
unknown <anshikamishra1906@gmail.com>
2026-01-15 22:45:57 +0530

    TiangleArea

---
+++ Basic-Programmes/TriangleArea.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class TriangleArea {
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+	System.out.print("Enter base: ");
+    double base = input.nextDouble();
+	System.out.print("Enter height: ");
+    double height = input.nextDouble();
+    double areaInInches = 0.5 * base * height;
+    double areaInCm = areaInInches * 2.54 * 2.54;
+    System.out.println("The area of the triangle is " + areaInInches + " square inches and " + areaInCm + " square centimeters");
+    }
+}
