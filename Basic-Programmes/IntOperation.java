379164bad165ccb8a091344d6e7776af1511169e
unknown <anshikamishra1906@gmail.com>
2026-01-15 23:46:07 +0530

    IntOperation

---
+++ Basic-Programmes/IntOperation.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class IntOperation{
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+	System.out.print("Enter a:");
+    int a = scanner.nextInt();
+	System.out.print("Enter b:");
+    int b = scanner.nextInt();
+	System.out.print("Enter c:");
+    int c = scanner.nextInt();
+    int result1 = a + b * c;
+    int result2 = a * b + c;
+    int result3 = c + a / b;
+    int result4 = a % b + c;
+    System.out.println("The results of Int Operations are " + result1 + ", " + result2 + ", " + result3 + ", " + result4);
+    }
+}
