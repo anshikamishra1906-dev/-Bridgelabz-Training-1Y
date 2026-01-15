08007fef1a262b41d688439f913638d4fdd348c6
unknown <anshikamishra1906@gmail.com>
2026-01-15 14:49:37 +0530

    commit

---
+++ Basic-Programmes/HarryAge.java
@@ -0,0 +1,8 @@
+public class HarryAge{
+    public static void main(String[] args){
+	int birthYr = 2000;
+	int currentYr = 2024;
+	int age = currentYr - birthYr;
+	System.out.println("Harry's age in 2024 is:"+age);
+	}
+}	
