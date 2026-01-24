03c36c64f54abec7ac60e48abcc9c7ad8308a9e3
unknown <anshikamishra1906@gmail.com>
2026-01-24 12:55:30 +0530

    LargestOf3.java

--- Basic-Programmes/LargestOf3.java
+++ Basic-Programmes/LargestOf3.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class LargestOf3{
+public class LargestOf3 {
     public static void main(String[] args){
 	Scanner scanner = new Scanner(System.in);
 	System.out.println("Enter first number:");
@@ -8,14 +8,26 @@ public class LargestOf3{
 	int num2 = scanner.nextInt();
 	System.out.println("Enter third number:");
 	int num3 = scanner.nextInt();
-	if (num1 > num2 && num1 > num3){
-	System.out.println(+num1+" is largest");
+	System.out.println("Is the first number the largest?");
+    if (num1 > num2 && num1 > num3){
+        System.out.println("YES!");
 	}
+    else {
+        System.out.println("NO!");
+    }
+    System.out.println("Is the second number the largest?");
 	if (num2 > num3 && num2 > num1){
-	System.out.println(+num2+" is largest");
+	System.out.println("YES!");
 	}
-	else{
-	System.out.println(+num3+" is largest");
+    else{
+    System.out.println("NO!");
+    }
+    System.out.println("Is the third number the largest?");
+	if (num3 > num1 && num3 > num2){
+	System.out.println("YES!");
 	}
+    else{
+    System.out.println("NO!");
+    }
 	}
 }
