85e58d9aa181f80ffc7a6e67570e3d2d0693fdb7
unknown <anshikamishra1906@gmail.com>
2026-04-13 16:29:52 +0530

    [Anshika Mishra]add . src folder with java files

---
+++ src/com/gla/package2/Sample2.java
@@ -0,0 +1,13 @@
+package com.gla.package2;
+
+import com.gla.package1.Sample1;
+
+public class Sample2 {
+    public static void main(String[] args){
+//        System.out.println(varB);
+        Sample1 obj = new Sample1();
+//        methodB();
+//        Sample1 obj2 = new Sample1();
+//        obj2.methodA();
+    }
+}
